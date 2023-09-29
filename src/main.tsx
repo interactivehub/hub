3cfb29dd7f6e4f2f3cf97bf990b16ac6492c4baa
ocintnaf <niccolo.fant@gmail.com>
2023-09-29 08:02:49 +0200

    add toaster

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client"
 import Home from "./home.tsx"
 import "./index.css"
 import { ThemeProvider } from "./components/theme-provider.tsx"
+import { Toaster } from "./components/ui/toaster.tsx"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
       <Home />
+      <Toaster />
     </ThemeProvider>
   </React.StrictMode>
 )
