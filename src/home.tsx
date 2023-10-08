70a972ec72dffd7ff46fc50e4632077d829eccc6
ocintnaf <niccolo.fant@gmail.com>
2023-10-08 10:11:51 +0200

    add new games soon alert

--- src/home.tsx
+++ src/home.tsx
@@ -1,37 +1,39 @@
-import React from "react"
-import useWebSocket from "react-use-websocket"
-import { useToast } from "./components/ui/use-toast"
-import { WSEvent } from "./types"
 import WheelLayout from "./games/wheel/components/wheel-layout"
 import Logo from "./components/logo"
 import { Toaster } from "./components/ui/toaster"
-
-const engineUrl = import.meta.env.ENGINE_WS_URL || "ws://localhost:8080/ws"
+import { useGamesMetadata } from "./games/hooks/use-games-metadata"
+import { Alert, AlertTitle } from "./components/ui/alert"
+import { RocketIcon } from "@radix-ui/react-icons"
 
 const Home = () => {
-  const { toast } = useToast()
+  const {
+    data: gamesMetadata,
+    isLoading: isGamesMetadataLoading,
+    error: gamesMetadataError,
+  } = useGamesMetadata()
 
-  const [newUsers, setNewUsers] = React.useState<WSEvent[]>([])
-
-  const { lastJsonMessage } = useWebSocket<WSEvent>(engineUrl)
-
-  React.useEffect(() => {
-    if (lastJsonMessage !== null) {
-      setNewUsers((prev) => prev.concat(lastJsonMessage))
-    }
-  }, [lastJsonMessage, toast])
-
-  console.log(newUsers)
+  if (isGamesMetadataLoading) return null
+  if (gamesMetadataError || !gamesMetadata) return null
 
   return (
     <div className="overflow-hidden h-screen">
-      <div className="h-20 p-4">
-        <Logo />
-      </div>
+      <header className="p-4">
+        <Alert className="bg-popover">
+          <RocketIcon className="h-4 w-4" />
+          <AlertTitle className="font-normal tracking-normal">
+            New games coming soon!
+          </AlertTitle>
+        </Alert>
+        <div className="h-20 py-4">
+          <Logo />
+        </div>
+      </header>
 
-      <WheelLayout />
+      <main>
+        <WheelLayout wheelMetadata={gamesMetadata.wheel} />
 
-      <Toaster />
+        <Toaster />
+      </main>
     </div>
   )
 }
